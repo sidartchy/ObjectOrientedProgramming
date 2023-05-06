f5e2b1124f2bebebc609ec34121a54abc3fc84c1
Siddharth Chaudhary <sidartchy203@gmail.com>
2023-05-06 21:13:09 +0545

    This Pointer

---
+++ tut30.cpp
@@ -0,0 +1,28 @@
+// This pointer in CPP
+#include <iostream>
+using namespace std;
+
+class A
+{
+    int a;
+
+public:
+    void setData(int a)
+    {
+        this->a = a; // using this keyword
+    }
+    void getData()
+    {
+        cout << "The value of a is " << a << endl;
+    }
+};
+
+int main()
+{
+
+    // 'this' is a keyword which is a pointer which points to the object which invokes the member function
+    A a;
+    a.setData(4);
+    a.getData();
+    return 0;
+}
