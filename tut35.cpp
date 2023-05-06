807b594c253f117dd9bff4fcac29077f69502d06
Siddharth Chaudhary <sidartchy203@gmail.com>
2023-05-06 21:16:32 +0545

    Pure virtual functions

---
+++ tut35.cpp
@@ -0,0 +1,89 @@
+// Pure Virtual Function----|  related
+// Abstract Base Class------|  related
+
+// prv program but with line 20 changed.
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class CWH
+{
+protected:
+    string title;
+    float rating;
+
+public:
+    CWH(string s, float r)
+    {
+        title = s;
+        rating = r;
+    }
+    virtual void display() = 0; // do nothing function --> pure virtual function
+};
+
+class CWHVideo : public CWH
+{
+    float videoLength;
+
+public:
+    CWHVideo(string s, float r, float vL) : CWH(s, r)
+    {
+        videoLength = vL;
+    }
+
+    void display()
+    {
+        cout << "This is an amazing video with title " << title << endl;
+        cout << "Ratings : " << rating << " out of 5 stars" << endl;
+        cout << "Length of this video is : " << videoLength << " minutes" << endl;
+    }
+};
+class CWHText : public CWH
+{
+    int words;
+
+public:
+    CWHText(string s, float r, int wc) : CWH(s, r)
+    {
+        words = wc;
+    }
+
+    void display()
+    {
+        cout << "This is an amazing article with title " << title << endl;
+        cout << "Ratings : " << rating << " out of 5 stars" << endl;
+        cout << "No of words in this text is  : " << words << " words" << endl;
+    }
+};
+
+int main()
+{
+
+    string title;
+    float rating, vlen;
+    int words;
+
+    // for code with harry video
+    title = "Django tutorial";
+    vlen = 4.56;
+    rating = 3.5;
+    CWHVideo djvideo(title, rating, vlen);
+    djvideo.display();
+
+    // for code with harry text
+    title = "Django tutorial Text";
+    words = 565;
+    rating = 4.0;
+    CWHText djtext(title, rating, words);
+    djtext.display();
+
+    CWH *tuts[2];
+    tuts[0] = &djvideo;
+    tuts[1] = &djtext;
+
+    tuts[0]->display(); // calls display() of video
+    tuts[1]->display(); // calls display() of text
+
+    return 0;
+}
